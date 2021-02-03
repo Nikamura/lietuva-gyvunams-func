111676bbb76afdb72e10bffddab8b53bc68fc013
Karolis Mažukna <me@karolis.dev>
2021-02-03 23:27:35 +0200

    use latest ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import { Telegraf } from "telegraf";
 
-const bot = new Telegraf(process.env.LIETUVA_GYVUNAMS_TELEGRAM_BOT_TOKEN);
+const BOT_TOKEN = process.env.LIETUVA_GYVUNAMS_TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.LIETUVA_GYVUNAMS_TELEGRAM_CHAT_ID;
+
+if (!BOT_TOKEN || !CHAT_ID) {
+  throw new Error(
+    "Missing LIETUVA_GYVUNAMS_TELEGRAM_BOT_TOKEN and/or LIETUVA_GYVUNAMS_TELEGRAM_CHAT_ID env variables."
+  );
+}
+
+const bot = new Telegraf(BOT_TOKEN);
 
 export default (request: NowRequest, response: NowResponse) => {
   const { query, body, headers } = request;
-  bot.telegram.sendMessage(
-    process.env.LIETUVA_GYVUNAMS_TELEGRAM_CHAT_ID,
-    JSON.stringify({ query, body, headers })
-  );
+  bot.telegram.sendMessage(CHAT_ID, JSON.stringify({ query, body, headers }));
   response.status(200).send(`Thanks! Come again!`);
 };
