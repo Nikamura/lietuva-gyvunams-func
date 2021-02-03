05ecdb231668d1bf0d2eeaacb19d67816bbc7c5a
Karolis Mažukna <me@karolis.dev>
2021-02-03 23:22:38 +0200

    send message to telegram

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,13 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf(process.env.LIETUVA_GYVUNAMS_TELEGRAM_BOT_TOKEN);
 
 export default (request: NowRequest, response: NowResponse) => {
-  const { name = "World" } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  const { query, body, headers } = request;
+  bot.telegram.sendMessage(
+    process.env.LIETUVA_GYVUNAMS_TELEGRAM_CHAT_ID,
+    JSON.stringify({ query, body, headers })
+  );
+  response.status(200).send(`Thanks! Come again!`);
 };
